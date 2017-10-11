a505109cac0414b2f4d5a423e5dc1bccb7323d33
Rameez Qurashi <rameez704@gmail.com>
2017-10-11 18:40:57 -0400

    Clean up Tank.h

--- Source/TankBattle/Public/Tank.h
+++ Source/TankBattle/Public/Tank.h
@@ -22,11 +22,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UTankAimingComponent* TankAimingComponent = NULL;
+public:
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UTankAimingComponent* TankAimingComponent = NULL;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;	

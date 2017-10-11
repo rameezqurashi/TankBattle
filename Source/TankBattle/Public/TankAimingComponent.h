e34539c5530ecabc092d12d90324504ff212a9d7
Rameez Qurashi <rameez704@gmail.com>
2017-10-11 17:28:41 -0400

    Clean up TankAimingComponent

--- Source/TankBattle/Public/TankAimingComponent.h
+++ Source/TankBattle/Public/TankAimingComponent.h
@@ -16,13 +16,7 @@ public:
 	// Sets default values for this component's properties
 	UTankAimingComponent();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
 public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	void AimAt(FVector & HitLocation, const float Launchspeed);
 

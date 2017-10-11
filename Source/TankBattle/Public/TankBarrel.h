efcccfb4e965f4d7faa698570fb62699f3488e07
Rameez Qurashi <rameez704@gmail.com>
2017-10-11 18:20:19 -0400

    Convert Barrel in blueprint to TankBarrel from StaticMesh

---
+++ Source/TankBattle/Public/TankBarrel.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankBarrel.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class TANKBATTLE_API UTankBarrel : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	void Elevate(float DegreesPerSecond);
+
+private:
+	UPROPERTY(EditAnywhere)
+	float MaxDegressPerSecond;
+
+	UPROPERTY(EditAnywhere)
+	float MaxElevationDegrees;
+
+	UPROPERTY(EditAnywhere)
+	float MinElevationDegrees;	
+};

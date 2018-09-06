151d628f20e6f2547fe7fb4c204f17f40b7a9afa
Rameez Qurashi <rameez704@gmail.com>
2018-09-06 18:48:49 -0400

    Tank reloading behavior works correctly now

--- Source/TankBattle/Private/TankAimingComponent.cpp
+++ Source/TankBattle/Private/TankAimingComponent.cpp
@@ -9,13 +9,20 @@
 // Sets default values for this component's properties
 UTankAimingComponent::UTankAimingComponent()
 {
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankAimingComponent::BeginPlay()
+{
+	LastFireTime = FPlatformTime::Seconds();
 }
 
 void UTankAimingComponent::AimAt(FVector & HitLocation)
 {
 	TArray<AActor*> IgnoreList = { GetOwner() };
 	FVector OutLaunchVelocity;
-	ensure(Barrel);
+	if (!ensure(Barrel))
+		return;
 
 	// Returns 0 if invalid path
 	if (UGameplayStatics::SuggestProjectileVelocity(this, 
@@ -31,14 +38,17 @@ void UTankAimingComponent::AimAt(FVector & HitLocation)
 													IgnoreList, 
 													false))
 	{
-		MoveBarrel(OutLaunchVelocity.GetSafeNormal());
+		AimDirection = OutLaunchVelocity.GetSafeNormal();
+		MoveBarrel(AimDirection);
 	}
 }
 
 void UTankAimingComponent::MoveBarrel(FVector AimDirection)
 {
-	ensure(Barrel);
-	ensure(Turret);
+	if (!ensure(Barrel))
+		return;
+	if (!ensure(Turret))
+		return;
 	FRotator BarrelRotation = Barrel->GetComponentRotation();
 	FRotator AimRotation = AimDirection.Rotation();
 	FRotator DeltaRotation = AimRotation - BarrelRotation;
@@ -64,9 +74,7 @@ void UTankAimingComponent::Fire()
 	if (!ensure(Barrel && ProjectileBlueprint))
 		return;
 
-	bool isReloaded = (FPlatformTime::Seconds() - LastFireTime) > ReloadTimeSeconds;
-
-	if (isReloaded)
+	if (FiringStatus != EFiringStatus::Reloading)
 	{
 		auto Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileBlueprint,
 			Barrel->GetSocketLocation(FName("Projectile")),
@@ -76,5 +84,33 @@ void UTankAimingComponent::Fire()
 		Projectile->LaunchProjectile(LaunchSpeed);
 
 		LastFireTime = FPlatformTime::Seconds();
+	}
+}
+
+bool UTankAimingComponent::IsBarrelMoving()
+{
+	if (ensure(Barrel))
+		return !(Barrel->GetForwardVector().Equals(AimDirection, .01));
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("NO BARREL IN IS BARREL MOVING METHOD"));
+		return false;
+	}
+
+}
+
+void UTankAimingComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	if ((FPlatformTime::Seconds() - LastFireTime) < ReloadTimeSeconds)
+	{
+		FiringStatus = EFiringStatus::Reloading;
+	}
+	else if (IsBarrelMoving())
+	{
+		FiringStatus = EFiringStatus::Aiming;
+	}
+	else
+	{
+		FiringStatus = EFiringStatus::Locked;
 	}
 }
--- Source/TankBattle/Public/TankAimingComponent.h
+++ Source/TankBattle/Public/TankAimingComponent.h
@@ -39,9 +39,17 @@ public:
 
 protected:
 	UPROPERTY(BlueprintReadOnly, Category = "State")
-	EFiringStatus FiringStatus = EFiringStatus::Aiming;
+	EFiringStatus FiringStatus = EFiringStatus::Reloading;
 
 private:
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+	virtual void BeginPlay() override;
+
+	bool IsBarrelMoving();
+
+	FVector AimDirection;
+
 	UPROPERTY(EditDefaultsOnly, Category = "Firing")
 	float LaunchSpeed;
 
